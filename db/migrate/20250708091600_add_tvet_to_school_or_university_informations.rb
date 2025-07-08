24b0f5db11f90fa46f85cbc0ff7b61047a417511
senaygui <senaygeletaw@gmail.com>
2025-07-08 12:33:31 +0300

    update

--- db/migrate/20250708091600_add_tvet_to_school_or_university_informations.rb
+++ db/migrate/20250708091600_add_tvet_to_school_or_university_informations.rb
@@ -2,7 +2,6 @@ class AddTvetToSchoolOrUniversityInformations < ActiveRecord::Migration[7.0]
   def change
     add_column :school_or_university_informations, :tvet_program_attend, :string
     add_column :school_or_university_informations, :coc_attended_date, :datetime
-    add_column :school_or_university_informations, :coc_id, :string
     add_column :school_or_university_informations, :tvet_level, :string
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2025_04_16_121401) do
+ActiveRecord::Schema[7.0].define(version: 2025_07_08_091600) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
   enable_extension "plpgsql"
@@ -1137,6 +1137,9 @@ ActiveRecord::Schema[7.0].define(version: 2025_04_16_121401) do
     t.string "tvet"
     t.string "letter_of_equivalence"
     t.string "entrance_exam_id"
+    t.string "tvet_program_attend"
+    t.datetime "coc_attended_date"
+    t.string "tvet_level"
     t.index ["student_id"], name: "index_school_or_university_informations_on_student_id"
   end
 
@@ -1505,6 +1508,28 @@ ActiveRecord::Schema[7.0].define(version: 2025_04_16_121401) do
     t.string "source_table", default: "grades", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "users", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer "sign_in_count", default: 0, null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string "current_sign_in_ip"
+    t.string "last_sign_in_ip"
+    t.string "first_name", null: false
+    t.string "last_name", null: false
+    t.string "middle_name"
+    t.string "nationality", null: false
+    t.string "company_name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   create_table "withdrawals", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
