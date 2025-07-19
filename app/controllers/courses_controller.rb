2d79911af78eaf2faca741eb239a27a454597a26
senaygui <senaygeletaw@gmail.com>
2025-07-19 21:18:40 +0300

    update

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,16 +1,18 @@
 class CoursesController < ApplicationController
     def index
-        @courses = Course.includes(:program, :curriculum).where(program: current_student.program, curriculum: current_student.program.curriculums, year: current_student.year, semester: current_student.semester)
-      end
-    
+        student_curriculum = current_student.program.curriculums.find_by(curriculum_version: current_student.curriculum_version)
+        @courses = Course.includes(:program, :curriculum)
+                         .where(program: current_student.program, curriculum: student_curriculum, year: current_student.year, semester: current_student.semester)
+    end
+
       # Other actions can be added here (show, new, edit, create, update, destroy)
 
       def pdf_by_scope
         scope = params[:scope]
         courses = filtered_courses(scope)
-    
+
         pdf = CoursesPdf.new(courses)
-    
+
         send_data pdf.render, filename: "courses_#{scope}.pdf", type: 'application/pdf', disposition: 'inline'
       end
 
@@ -30,7 +32,7 @@ class CoursesController < ApplicationController
           render :edit, notice: 'Assessment Plan NOT created.' # Adjust as necessary
         end
       end
-    
+
       def update_assessment_plan
         @assessment_plan = @course.assessment_plans.find(params[:id])
         if @assessment_plan.update(assessment_plan_params)
@@ -42,7 +44,7 @@ class CoursesController < ApplicationController
 
       private
 
-      #def filtered_courses(scope)
+      # def filtered_courses(scope)
       #  case scope
       #  when 'regular_year_1_semester_1'
       #    Course.joins(:program).where(year: 1, semester: 1, programs: { admission_type: 'regular' })
@@ -52,17 +54,19 @@ class CoursesController < ApplicationController
       #  else
       #    Course.none
       #  end
-      #end
-    
+      # end
+
       def set_course
         @course = Course.find(params[:id])
       end
-    
+
       def course_params
-        params.require(:course).permit(:course_module_id, :curriculum_id, :program_id, :course_title, :course_code, :course_description, :year, :semester, :course_starting_date, :course_ending_date, :credit_hour, :lecture_hour, :lab_hour, :ects, :created_by, :last_updated_by, :major, :batch)
+        params.require(:course).permit(:course_module_id, :curriculum_id, :program_id, :course_title, :course_code,
+                                       :course_description, :year, :semester, :course_starting_date, :course_ending_date, :credit_hour, :lecture_hour, :lab_hour, :ects, :created_by, :last_updated_by, :major, :batch)
       end
 
       def assessment_plan_params
-        params.require(:assessment_plan).permit(:assessment_title, :assessment_weight, :final_exam, :created_by, :updated_by, :admin_user_id)
+        params.require(:assessment_plan).permit(:assessment_title, :assessment_weight, :final_exam, :created_by,
+                                                :updated_by, :admin_user_id)
       end
 end
